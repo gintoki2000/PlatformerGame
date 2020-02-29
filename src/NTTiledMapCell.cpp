db496c3de077d180d38e72642b66b2debfe29c4a
gintoki2000 <ngotrungaccor@gmail.com>
2020-02-29 16:13:51 +0700

    fixed deleting unowned object: NTTiledMapCell::m_tile

--- src/NTTiledMapCell.cpp
+++ src/NTTiledMapCell.cpp
@@ -6,15 +6,12 @@ NTTiledMapCell::NTTiledMapCell(NTTiledMapTile *tile) : m_tile(tile) {}
 
 NTTiledMapCell::~NTTiledMapCell()
 {
-		delete m_tile;
 }
 
 NTTiledMapTile *NTTiledMapCell::getTile() { return m_tile; }
 
 void NTTiledMapCell::setTile(NTTiledMapTile *tile)
 {
-		if(m_tile != nullptr)
-				delete m_tile;
 		m_tile = tile;
 }
 
