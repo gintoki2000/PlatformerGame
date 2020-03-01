158d2e263f236a30f4897112b557d2f9db399208
gintoki2000 <ngotrungaccor@gmail.com>
2020-03-01 14:26:53 +0700

    format NTTileLayerCell.cpp

--- src/NTTileLayerCell.cpp
+++ src/NTTileLayerCell.cpp
@@ -2,17 +2,10 @@
 #include "NTTileLayerTile.h"
 NTTileLayerCell::NTTileLayerCell() : NTTileLayerCell(nullptr) {}
 
-NTTileLayerCell::NTTileLayerCell(NTTileLayerTile *tile) : m_tile(tile) {}
+NTTileLayerCell::NTTileLayerCell(NTTileLayerTile* tile) : m_tile(tile) {}
 
-NTTileLayerCell::~NTTileLayerCell()
-{
-}
+NTTileLayerCell::~NTTileLayerCell() {}
 
-NTTileLayerTile *NTTileLayerCell::getTile() { return m_tile; }
+NTTileLayerTile* NTTileLayerCell::getTile() { return m_tile; }
 
-void NTTileLayerCell::setTile(NTTileLayerTile *tile)
-{
-		m_tile = tile;
-}
-
-
+void NTTileLayerCell::setTile(NTTileLayerTile* tile) { m_tile = tile; }
