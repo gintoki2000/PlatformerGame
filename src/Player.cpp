0e2d4a29814df6d29303dd3be5911e9fe7650409
gintoki2000 <ngotrungaccor@gmail.com>
2020-03-04 18:17:30 +0700

    create animations, handle states

--- src/Player.cpp
+++ src/Player.cpp
@@ -7,12 +7,11 @@
 #include "NTTextureRegion.h"
 #include "SDL_error.h"
 #include "SDL_image.h"
+#include "SDL_keyboard.h"
 #include "SDL_render.h"
-Player::Player() :
-    m_isOnGround(false), m_animationState(ANIMATION_IDLE_1), m_body(nullptr),
-    m_level(nullptr)
-{
-}
+#include "SDL_scancode.h"
+
+Player::Player() : m_body(nullptr), m_level(nullptr) {}
 
 Player::~Player()
 {
@@ -21,18 +20,23 @@ Player::~Player()
 }
 
 static std::vector<NTTextureRegion>
-create_sprite_sheet(SDL_Texture* texture, int startX, int startY, int rows,
-                    int cols, int spriteWidth, int spriteHeight)
+createSpriteSheet(SDL_Texture* texture, int startX, int startY, int rows,
+                  int cols, int spriteWidth, int spriteHeight)
 {
-    std::vector<NTTextureRegion> spriteSheet(rows * cols);
+    std::vector<NTTextureRegion> spriteSheet;
+    spriteSheet.reserve((unsigned long)(rows * cols));
     SDL_Rect rect{startX, startY, spriteWidth, spriteHeight};
-    for (int i = 0; i < rows; ++i)
-        for (int j = 0; j < cols; ++j)
+    for (int y = 0; y < rows; ++y)
+    {
+        for (int x = 0; x < cols; ++x)
         {
-            rect.x = startX + j * spriteWidth;
-            rect.y = startY + i * spriteHeight;
-            spriteSheet[i * rows + j] = NTTextureRegion(texture, rect);
+            rect.x = x * spriteWidth + startX;
+            rect.y = y * spriteHeight + startY;
+            rect.w = spriteWidth;
+            rect.h = spriteHeight;
+            spriteSheet.emplace_back(texture, rect);
         }
+    }
     return spriteSheet;
 }
 
@@ -47,55 +51,580 @@ Player* Player::create(Level* level)
 
 bool Player::initialize(Level* level)
 {
+    return initialize(level, b2Vec2(Game::WIDTH / 2.f, Game::HEIGHT / 2.f));
+}
 
-    SDL_Surface* surface = IMG_Load("asserts/player.png");
-    if (surface == nullptr)
+bool Player::initialize(Level* level, const b2Vec2& position)
+{
+    if ((m_texture = IMG_LoadTexture(Game::getInstance()->getRenderer(),
+                                     "asserts/player.png")) == nullptr)
     {
-        SDL_Log("Could not load texture: %s", SDL_GetError());
+        SDL_Log("Could not load texture: %s", IMG_GetError());
         return false;
     }
-
-    m_texture = SDL_CreateTextureFromSurface(Game::getInstance()->getRenderer(),
-                                             surface);
-
-    m_animations[ANIMATION_IDLE_1] = Animation<NTTextureRegion>(
-        create_sprite_sheet(m_texture, 0, 0, 1, 4, SPRITE_WIDTH, SPRITE_HEIGHT),
-        1.f, ANIMATION_TYPE_NORMAL);
-    m_animations[ANIMATION_IDLE_2] = Animation<NTTextureRegion>(
-        create_sprite_sheet(m_texture, 100, 185, 1, 4, SPRITE_WIDTH,
-                            SPRITE_HEIGHT),
-        1.f, ANIMATION_TYPE_LOOP);
-
     m_level = level;
+    m_timer = 0.f;
+    m_state = STATE_IDLE_1;
+    m_direction = DIRECTION_LEFT;
+    m_isOnGround = false;
+    m_continueAttack = false;
 
+    createAnimations();
+    createBody(position);
+    return true;
+}
+
+void Player::update(float dt)
+{
+    m_isOnGround = false;
+    m_isWallSliding = false;
+    updateKeyState(dt);
+    checkWallSliding();
+    checkOnGround();
+    SDL_Log("is_wall_sliding=%d, is_on_ground = %d", m_isWallSliding, m_isOnGround);
+    int inputDirection = 0;
+    if (isButtonLeftPressed())
+    {
+        inputDirection -= 1;
+    }
+    if (isButtonRightPressed())
+    {
+        inputDirection += 1;
+    }
+    if (inputDirection > 0)
+    {
+        m_direction = DIRECTION_RIGHT;
+    }
+    if (inputDirection < 0)
+    {
+        m_direction = DIRECTION_LEFT;
+    }
+    switch (m_state)
+    {
+    case STATE_IDLE_1:
+    {
+        m_timer += dt;
+        if (!m_isOnGround)
+        {
+            changeState(STATE_FALL);
+            break;
+        }
+        if (isButtonBPressed() && m_isOnGround)
+        {
+            jump();
+            break;
+        }
+        if (isButtonAJustPressed())
+        {
+            changeState(STATE_ATTACK_1);
+            break;
+        }
+        if (inputDirection != 0)
+        {
+            move(inputDirection * 1.f);
+            changeState(STATE_RUN);
+            break;
+        }
+
+        break;
+    }
+    case STATE_IDLE_2:
+    {
+        m_timer += dt;
+        if (isButtonBPressed() && m_isOnGround)
+        {
+            jump();
+            break;
+        }
+
+        if (isButtonAJustPressed())
+        {
+            changeState(STATE_ATTACK_1);
+            break;
+        }
+
+        if (inputDirection != 0)
+        {
+            move(inputDirection * 1.f);
+            changeState(STATE_RUN);
+            break;
+        }
+        if (m_timer >= 3.f)
+        {
+            changeState(STATE_IDLE_1);
+            break;
+        }
+        break;
+    }
+    case STATE_RUN:
+    {
+        m_timer += dt;
+        if (isButtonBPressed() && m_isOnGround)
+        {
+            jump();
+            break;
+        }
+        if (inputDirection != 0)
+        {
+            move(inputDirection * 1.f);
+        }
+        else
+        {
+            setZeroVelX();
+            changeState(STATE_IDLE_2);
+        }
+        break;
+    }
+    case STATE_JUMP:
+    {
+        m_timer += dt;
+
+        if (m_isWallSliding &&
+            ((m_direction == DIRECTION_LEFT && isButtonLeftPressed()) ||
+             (m_direction == DIRECTION_RIGHT && isButtonRightPressed())))
+        {
+            changeState(STATE_WALL_SLIDE);
+            break;
+        }
+        if (inputDirection != 0)
+        {
+            move(inputDirection * 1.f);
+        }
+        else
+        {
+            setZeroVelX();
+        }
+        if (isCurrentAnimationComplete())
+        {
+            changeState(STATE_SMRSLT);
+            break;
+        }
+        break;
+    }
+    case STATE_SMRSLT:
+    {
+        m_timer += dt;
+
+        if (isButtonAJustPressed())
+        {
+            changeState(STATE_AIR_ATTACK_1);
+            break;
+        }
+        if (m_isWallSliding &&
+            ((m_direction == DIRECTION_LEFT && isButtonLeftPressed()) ||
+             (m_direction == DIRECTION_RIGHT && isButtonRightPressed())))
+        {
+            changeState(STATE_WALL_SLIDE);
+            break;
+        }
+        if (inputDirection != 0)
+        {
+            move(inputDirection * 1.f);
+        }
+        else
+        {
+            setZeroVelX();
+        }
+        if (isCurrentAnimationComplete())
+        {
+            changeState(STATE_FALL);
+            break;
+        }
+
+        break;
+    }
+    case STATE_FALL:
+    {
+        m_timer += dt;
+
+        if (m_isWallSliding &&
+            ((m_direction == DIRECTION_LEFT && isButtonLeftPressed()) ||
+             (m_direction == DIRECTION_RIGHT && isButtonRightPressed())))
+        {
+            changeState(STATE_WALL_SLIDE);
+            break;
+        }
+        if (inputDirection != 0)
+        {
+            move(inputDirection * 1.f);
+        }
+        else
+        {
+            setZeroVelX();
+        }
+        if (m_isOnGround)
+        {
+            changeState(STATE_IDLE_2);
+        }
+        if (isButtonAJustPressed())
+        {
+            changeState(STATE_AIR_ATTACK_1);
+            break;
+        }
+        break;
+    }
+    case STATE_ATTACK_1:
+    {
+        m_timer += dt;
+        if (isButtonAJustPressed())
+        {
+            m_continueAttack = true;
+        }
+        if (isCurrentAnimationComplete())
+        {
+            if (m_continueAttack)
+            {
+                changeState(STATE_ATTACK_2);
+                m_continueAttack = false;
+            }
+            else
+                changeState(STATE_IDLE_2);
+            break;
+        }
+        break;
+    }
+    case STATE_ATTACK_2:
+    {
+        m_timer += dt;
+        if (isButtonAJustPressed())
+        {
+            m_continueAttack = true;
+        }
+        if (isCurrentAnimationComplete())
+        {
+            if (m_continueAttack)
+            {
+                changeState(STATE_ATTACK_3);
+                m_continueAttack = false;
+            }
+            else
+                changeState(STATE_IDLE_2);
+            break;
+        }
+        break;
+    }
+    case STATE_ATTACK_3:
+    {
+        m_timer += dt;
+
+        if (isCurrentAnimationComplete())
+        {
+            changeState(STATE_IDLE_2);
+            break;
+        }
+        break;
+    }
+    case STATE_AIR_ATTACK_1:
+    {
+        m_timer += dt;
+        if (isButtonAJustPressed())
+        {
+            m_continueAttack = true;
+        }
+        if (isCurrentAnimationComplete())
+        {
+            if (m_continueAttack)
+            {
+                m_continueAttack = false;
+                changeState(STATE_AIR_ATTACK_2);
+            }
+            else
+            {
+                if (m_isOnGround)
+                {
+                    changeState(STATE_IDLE_2);
+                }
+                else
+                {
+                    changeState(STATE_FALL);
+                }
+            }
+        }
+        break;
+    }
+    case STATE_AIR_ATTACK_2:
+    {
+        m_timer += dt;
+
+        if (isButtonDownPressed() && isButtonAPressed())
+        {
+            m_continueAttack = true;
+        }
+        if (isCurrentAnimationComplete())
+        {
+
+            if (m_continueAttack)
+            {
+                m_continueAttack = false;
+                m_body->ApplyLinearImpulseToCenter(b2Vec2(0.f, 5.f), true);
+                changeState(STATE_AIR_ATTACK_3_LOOP);
+            }
+            else
+            {
+                if (m_isOnGround)
+                {
+                    changeState(STATE_IDLE_2);
+                }
+                else
+                {
+                    changeState(STATE_FALL);
+                }
+            }
+        }
+        break;
+    }
+    case STATE_AIR_ATTACK_3_RDY:
+    {
+        m_timer += dt;
+        if (isCurrentAnimationComplete())
+        {
+            changeState(STATE_AIR_ATTACK_3_END);
+        }
+        break;
+    }
+    case STATE_AIR_ATTACK_3_LOOP:
+    {
+        m_timer += dt;
+        if (m_isOnGround)
+        {
+            changeState(STATE_AIR_ATTACK_3_RDY);
+        }
+        break;
+    }
+    case STATE_AIR_ATTACK_3_END:
+    {
+        m_timer += dt;
+        if (isCurrentAnimationComplete())
+        {
+            changeState(STATE_IDLE_2);
+        }
+    }
+    case STATE_WALL_SLIDE:
+    {
+        m_timer += dt;
+        if (isButtonBJustPressed())
+        {
+            float sign = m_direction == DIRECTION_RIGHT ? 1.f : -1.f;
+            setZeroVelX();
+            m_body->ApplyLinearImpulseToCenter(b2Vec2(sign * -10.f, -15.f),
+                                               true);
+            changeState(STATE_JUMP);
+			m_isWallSliding = false;
+            break;
+        }
+        if (!m_isWallSliding ||
+            (m_direction == DIRECTION_LEFT && !isButtonLeftPressed()) ||
+            (m_direction == DIRECTION_RIGHT && !isButtonRightPressed()))
+        {
+            m_isWallSliding = false;
+            changeState(STATE_FALL);
+        }
+    }
+    break;
+    }
+}
+
+void Player::draw(SDL_Renderer* renderer, const NTRect& viewPort)
+{
+    Animation<NTTextureRegion>& animation = m_animations[m_state];
+    const NTTextureRegion& currentFrame = animation.getCurrentFrame(m_timer);
+    int drawX = m_body->GetPosition().x * Constances::PPM - SPRITE_WIDTH / 2 -
+                viewPort.x;
+    int drawY = m_body->GetPosition().y * Constances::PPM - SPRITE_HEIGHT / 2 -
+                viewPort.y;
+    currentFrame.draw(renderer, drawX, drawY,
+                      m_direction == DIRECTION_LEFT ? SDL_FLIP_HORIZONTAL
+                                                    : SDL_FLIP_NONE);
+}
+
+void Player::changeState(State newState)
+{
+    if (m_state == newState)
+        return;
+    m_timer = 0.f;
+    m_state = newState;
+}
+
+void Player::createBody(const b2Vec2& position)
+{
     b2BodyDef bodyDef;
     bodyDef.type = b2_dynamicBody;
     bodyDef.fixedRotation = true;
     bodyDef.userData = this;
-    bodyDef.position.x = Game::WIDTH / 2.f / Constances::PPM;
-    bodyDef.position.y = Game::HEIGHT / 2.f / Constances::PPM;
-
-    m_body = level->getWorld()->CreateBody(&bodyDef);
-
+    bodyDef.position.x = position.x / Constances::PPM;
+    bodyDef.position.y = position.y / Constances::PPM;
+    m_body = m_level->getWorld()->CreateBody(&bodyDef);
     b2PolygonShape shape;
     shape.SetAsBox(WIDTH / 2.f / Constances::PPM,
                    HEIGHT / 2.f / Constances::PPM);
     b2FixtureDef fixtureDef;
     fixtureDef.userData = this;
     fixtureDef.shape = &shape;
-
-    m_body->CreateFixture(&fixtureDef);
-
-    return true;
+    m_fixture = m_body->CreateFixture(&fixtureDef);
 }
 
-void Player::update(float dt) { m_animationTimeState += dt; }
-
-void Player::draw(SDL_Renderer* renderer, const NTRect& viewPort)
+static std::vector<NTTextureRegion>
+createSpriteSheet(SDL_Texture* texture, int startIndex, int cols, int count,
+                  int spriteWidth, int spriteHeight)
 {
-    const NTTextureRegion& currentFrame =
-        m_animations[m_animationState].getCurrentFrame(m_animationTimeState);
-    currentFrame.draw(renderer,
-        m_body->GetPosition().x * Constances::PPM - SPRITE_WIDTH / 2,
-        m_body->GetPosition().y * Constances::PPM - SPRITE_HEIGHT);
+    SDL_Rect r;
+    r.w = spriteWidth;
+    r.h = spriteHeight;
+    std::vector<NTTextureRegion> spriteSheet;
+    spriteSheet.reserve(count);
+    for (int i = startIndex; i < startIndex + count; ++i)
+    {
+        int col = i % cols;
+        int row = i / cols;
+        r.x = col * spriteWidth;
+        r.y = row * spriteHeight;
+        spriteSheet.emplace_back(texture, r);
+    }
+    return spriteSheet;
+}
+
+void Player::createAnimations()
+{
+    m_animations[STATE_IDLE_1] = Animation<NTTextureRegion>(
+        createSpriteSheet(m_texture, 0, 0, 1, 4, SPRITE_WIDTH, SPRITE_HEIGHT),
+        1.f / 6.f, ANIMATION_TYPE_LOOP);
+    m_animations[STATE_IDLE_2] = Animation<NTTextureRegion>(
+        createSpriteSheet(m_texture, 150, 185, 1, 4, SPRITE_WIDTH,
+                          SPRITE_HEIGHT),
+        1.f / 8.f, ANIMATION_TYPE_LOOP);
+    m_animations[STATE_JUMP] = Animation<NTTextureRegion>(
+        createSpriteSheet(m_texture, 0, 74, 1, 4, SPRITE_WIDTH, SPRITE_HEIGHT),
+        1.f / 10.f, ANIMATION_TYPE_NORMAL);
+    m_animations[STATE_SMRSLT] = Animation<NTTextureRegion>(
+        createSpriteSheet(m_texture, 18, 7, 4, SPRITE_WIDTH, SPRITE_HEIGHT),
+        1.f / 12.f, ANIMATION_TYPE_NORMAL);
+    m_animations[STATE_FALL] = Animation<NTTextureRegion>(
+        createSpriteSheet(m_texture, 50, 111, 1, 2, SPRITE_WIDTH,
+                          SPRITE_HEIGHT),
+        1.f / 10.f, ANIMATION_TYPE_LOOP);
+    m_animations[STATE_RUN] = Animation<NTTextureRegion>(
+        createSpriteSheet(m_texture, 50, 37, 1, 6, SPRITE_WIDTH, SPRITE_HEIGHT),
+        1.f / 10.f, ANIMATION_TYPE_LOOP);
+    m_animations[STATE_ATTACK_1] = Animation<NTTextureRegion>(
+        createSpriteSheet(m_texture, 0, 222, 1, 5, SPRITE_WIDTH, SPRITE_HEIGHT),
+        1.f / 10.f, ANIMATION_TYPE_NORMAL);
+    m_animations[STATE_ATTACK_2] = Animation<NTTextureRegion>(
+        createSpriteSheet(m_texture, 47, 7, 6, SPRITE_WIDTH, SPRITE_HEIGHT),
+        1.f / 10.f, ANIMATION_TYPE_NORMAL);
+    m_animations[STATE_ATTACK_3] = Animation<NTTextureRegion>(
+        createSpriteSheet(m_texture, 53, 7, 6, SPRITE_WIDTH, SPRITE_HEIGHT),
+        1.f / 10.f, ANIMATION_TYPE_NORMAL);
+    m_animations[STATE_AIR_ATTACK_1] = Animation<NTTextureRegion>(
+        createSpriteSheet(m_texture, 96, 7, 3, SPRITE_WIDTH, SPRITE_HEIGHT),
+        1.f / 8.f, ANIMATION_TYPE_NORMAL);
+    m_animations[STATE_AIR_ATTACK_2] = Animation<NTTextureRegion>(
+        createSpriteSheet(m_texture, 99, 7, 3, SPRITE_WIDTH, SPRITE_HEIGHT),
+        1.f / 8.f, ANIMATION_TYPE_NORMAL);
+    m_animations[STATE_AIR_ATTACK_3_LOOP] = Animation<NTTextureRegion>(
+        createSpriteSheet(m_texture, 103, 7, 2, SPRITE_WIDTH, SPRITE_HEIGHT),
+        1.f / 8.f, ANIMATION_TYPE_LOOP);
+    m_animations[STATE_AIR_ATTACK_3_RDY] = Animation<NTTextureRegion>(
+        createSpriteSheet(m_texture, 105, 7, 1, SPRITE_WIDTH, SPRITE_HEIGHT),
+        1.f / 8.f, ANIMATION_TYPE_NORMAL);
+    m_animations[STATE_AIR_ATTACK_3_END] = Animation<NTTextureRegion>(
+        createSpriteSheet(m_texture, 106, 7, 3, SPRITE_WIDTH, SPRITE_HEIGHT),
+        1.f / 8.f, ANIMATION_TYPE_NORMAL);
+    m_animations[STATE_WALL_SLIDE] = Animation<NTTextureRegion>(
+        createSpriteSheet(m_texture, 79, 7, 2, SPRITE_WIDTH, SPRITE_HEIGHT),
+        1.f / 10.f, ANIMATION_TYPE_LOOP);
+}
+
+void Player::checkOnGround()
+{
+    b2AABB aabb;
+    b2Vec2 halfSize(WIDTH / 2.f / Constances::PPM,
+                    HEIGHT / 2.f / Constances::PPM);
+    b2Vec2 footPos = m_body->GetPosition();
+    footPos.y += halfSize.y;
+
+    aabb.upperBound.x = footPos.x - halfSize.x + 1.f / Constances::PPM;
+    aabb.upperBound.y = footPos.y;
+
+    aabb.lowerBound.x = footPos.x + halfSize.x - 1.f / Constances::PPM;
+    aabb.lowerBound.y = footPos.y + 2.f / Constances::PPM;
+    m_checkingStatus = CHECK_GROUND;
+    m_level->getWorld()->QueryAABB(this, aabb);
+}
+
+void Player::checkWallSliding()
+{
+    b2AABB sensor;
+    b2Vec2 halfSize(WIDTH / 2.f / Constances::PPM,
+                    HEIGHT / 2.f / Constances::PPM);
+    b2Vec2 position = m_body->GetPosition();
+    if (m_direction == DIRECTION_LEFT)
+    {
+        sensor.upperBound.x = position.x - halfSize.x - 2.f / Constances::PPM;
+        sensor.upperBound.y = position.y - halfSize.y + 1.f / Constances::PPM;
+
+        sensor.lowerBound.x = position.x - halfSize.x;
+        sensor.lowerBound.y = position.y;
+    }
+    else
+    {
+        sensor.upperBound.x = position.x + halfSize.x;
+        sensor.upperBound.y = position.y - halfSize.y + 1.f / Constances::PPM;
+
+        sensor.lowerBound.x = position.x + halfSize.x + 2.f / Constances::PPM;
+        sensor.lowerBound.y = position.y;
+    }
+    m_checkingStatus = CHECK_SLIDE;
+    m_level->getWorld()->QueryAABB(this, sensor);
+}
+
+bool Player::ReportFixture(b2Fixture* f)
+{
+    if (f != m_fixture)
+    {
+        if (m_checkingStatus == CHECK_GROUND)
+            m_isOnGround = true;
+        else if (m_checkingStatus == CHECK_SLIDE)
+            m_isWallSliding = true;
+        return true;
+    }
+    return false;
+}
+
+void Player::jump()
+{
+    m_body->ApplyLinearImpulseToCenter(b2Vec2(0.f, -12.f), true);
+    changeState(STATE_JUMP);
+    m_isOnGround = false;
+}
+
+bool Player::isCurrentAnimationComplete()
+{
+    return m_animations[m_state].isComplete(m_timer);
+}
+
+void Player::move(float vx)
+{
+    m_body->ApplyLinearImpulseToCenter(b2Vec2(vx, 0.f), true);
+    b2Vec2 v = m_body->GetLinearVelocity();
+    v.x = std::max(-10.f, std::min(10.f, v.x));
+    m_body->SetLinearVelocity(v);
+}
+
+void Player::setZeroVelX()
+{
+    b2Vec2 v = m_body->GetLinearVelocity();
+    v.x = 0;
+    m_body->SetLinearVelocity(v);
+}
+
+void Player::updateKeyState(float)
+{
+    const Uint8* keyState = SDL_GetKeyboardState(nullptr);
+    m_prevKeyState = m_currentKeyState;
+    m_currentKeyState.buttonA = keyState[SDL_SCANCODE_X];
+    m_currentKeyState.buttonB = keyState[SDL_SCANCODE_Z];
+    m_currentKeyState.buttonUp = keyState[SDL_SCANCODE_UP];
+    m_currentKeyState.buttonDown = keyState[SDL_SCANCODE_DOWN];
+    m_currentKeyState.buttonLeft = keyState[SDL_SCANCODE_LEFT];
+    m_currentKeyState.buttonRight = keyState[SDL_SCANCODE_RIGHT];
 }
--- src/Player.h
+++ src/Player.h
@@ -1,64 +1,157 @@
 #ifndef PLAYER_H
 #define PLAYER_H
-#include "NTLayer.h"
-#include "Box2D/Box2D.h"
 #include "Animation.h"
+#include "NTLayer.h"
 #include "NTTextureRegion.h"
+#include "box2d/box2d.h"
 class Level;
-class Player : public NTLayer
+class Player : public b2QueryCallback
 {
   private:
-    enum AnimationState
+    enum State
     {
-        ANIMATION_IDLE_1,
-        ANIMATION_IDLE_2,
-        ANIMATION_RUN,
-        ANIMATION_JUMP,
-        ANIMATION_GRAB,
-        ANIMATION_CLIMB,
-        ANIMATION_HURT,
-        ANIMATION_FALL,
-        ANIMATION_ATTACK_1,
-        ANIMATION_ATTACK_2,
-        ANIMATION_ATTACK_3,
-        ANIMATION_AIR_ATTACK_1,
-        ANIMATION_AIR_ATTACK_2,
-        ANIMATION_AIR_ATTACK_3,
-        NUM_ANIMATIONS
+        STATE_IDLE_1,
+        STATE_IDLE_2,
+        STATE_RUN,
+        STATE_JUMP,
+        STATE_SMRSLT,
+        STATE_GRAB,
+        STATE_CLIMB,
+        STATE_HURT,
+        STATE_FALL,
+        STATE_ATTACK_1,
+        STATE_ATTACK_2,
+        STATE_ATTACK_3,
+        STATE_AIR_ATTACK_1,
+        STATE_AIR_ATTACK_2,
+        STATE_AIR_ATTACK_3_LOOP,
+        STATE_AIR_ATTACK_3_RDY,
+        STATE_AIR_ATTACK_3_END,
+        STATE_WALL_SLIDE,
+        NUM_STATES
+    };
+
+    enum Direction
+    {
+        DIRECTION_NONE,
+        DIRECTION_LEFT,
+        DIRECTION_RIGHT
     };
 
   public:
-    static constexpr float WIDTH = 50.f;
+    static constexpr float WIDTH = 20.f;
     static constexpr float HEIGHT = 37.f;
-	static constexpr int SPRITE_WIDTH = 50;
-	static constexpr int SPRITE_HEIGHT = 37;
+    static constexpr int SPRITE_WIDTH = 50;
+    static constexpr int SPRITE_HEIGHT = 37;
 
   public:
     static Player* create(Level* level);
 
-    ~Player() override;
+    ~Player();
 
-    void update(float dt) override;
+    void update(float dt);
 
-    void draw(SDL_Renderer* renderer, const NTRect& viewPort) override;
+    void draw(SDL_Renderer* renderer, const NTRect& viewPort);
 
   private:
     Player();
 
     bool initialize(Level* level);
 
+    bool initialize(Level* level, const b2Vec2& position);
+
+    void changeState(State newState);
+
+    void createBody(const b2Vec2& position);
+
+    void createAnimations();
+
+    void checkOnGround();
+
+	void checkWallSliding();
+
+    bool ReportFixture(b2Fixture* f);
+
+    void jump();
+
+    bool isCurrentAnimationComplete();
+
+    void move(float vx);
+
+    void setZeroVelX();
+
+    void updateKeyState(float dt);
+
+    bool isButtonAPressed() { return m_currentKeyState.buttonA; }
+    bool isButtonBPressed() { return m_currentKeyState.buttonB; }
+    bool isButtonUpPressed() { return m_currentKeyState.buttonUp; }
+    bool isButtonDownPressed() { return m_currentKeyState.buttonDown; }
+    bool isButtonLeftPressed() { return m_currentKeyState.buttonLeft; }
+    bool isButtonRightPressed() { return m_currentKeyState.buttonRight; }
+    bool isButtonAJustPressed()
+    {
+        return m_currentKeyState.buttonA && !m_prevKeyState.buttonA;
+    }
+    bool isButtonBJustPressed()
+    {
+        return m_currentKeyState.buttonB && !m_prevKeyState.buttonB;
+    }
+    bool isButtonUpJustPressed()
+    {
+        return m_currentKeyState.buttonUp && !m_prevKeyState.buttonUp;
+    }
+
+    bool isButtonDownJustPressed()
+    {
+        return m_currentKeyState.buttonDown && !m_prevKeyState.buttonDown;
+    }
+
+    bool isButtonLeftJustPressed()
+    {
+        return m_currentKeyState.buttonLeft && !m_prevKeyState.buttonLeft;
+    }
+
+    bool isButtonRightJustPressed()
+    {
+        return m_currentKeyState.buttonRight && !m_prevKeyState.buttonRight;
+    }
+
     bool m_isOnGround;
 
-    AnimationState m_animationState;
+    float m_timer;
 
-	float m_animationTimeState;
+    int m_direction;
+
+    State m_state;
 
     b2Body* m_body;
 
+    b2Fixture* m_fixture;
+
     Level* m_level;
 
-	Animation<NTTextureRegion> m_animations[NUM_ANIMATIONS];		
+    Animation<NTTextureRegion> m_animations[NUM_STATES];
 
-	SDL_Texture* m_texture;
+    SDL_Texture* m_texture;
+
+    bool m_continueAttack;
+
+    struct KeyState
+    {
+        bool buttonA = false;
+        bool buttonB = false;
+        bool buttonUp = false;
+        bool buttonDown = false;
+        bool buttonLeft = false;
+        bool buttonRight = false;
+    } m_prevKeyState, m_currentKeyState;
+
+    enum
+    {
+        CHECK_GROUND,
+        CHECK_SLIDE
+    } m_checkingStatus;
+
+	bool m_isWallSliding;
 };
 #endif // PLAYER_H
