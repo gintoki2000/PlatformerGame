b5dea72f287696d39930f010d6c9e93b567937c5
gintoki2000 <ngotrungaccor@gmail.com>
2020-03-16 16:49:19 +0700

    merge GameState class from comop

--- src/GameState.h
+++ src/GameState.h
@@ -1,15 +1,15 @@
 #ifndef GAME_STATE_H
 #define GAME_STATE_H
-#include "SDL_render.h"
-class GameState 
+class GameState
 {
-	public:
-			GameState() {}
+  public:
+    GameState() {}
+    virtual ~GameState() {}
 
-			virtual ~GameState() {}
-
-			virtual void update(float dt) = 0;
-
-			virtual void draw(SDL_Renderer* renderer) = 0;
+    virtual void render(float deltaTime) = 0;
+    virtual void show() = 0;
+    virtual void hidden() = 0;
+    virtual void paused() = 0;
+    virtual void resume() = 0;
 };
-#endif //GAME_STATE_H
+#endif // GAME_STATE_H
